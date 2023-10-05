39b354725c4bd9ff32eb04047c08c604778599f5
Kaspersky <103287853+Kaspersky2500@users.noreply.github.com>
2023-10-05 22:25:16 +0300

    Update security.lua

--- Kaspersky-Lua-Security/lua/tabs/security.lua
+++ Kaspersky-Lua-Security/lua/tabs/security.lua
@@ -2,7 +2,7 @@ function SecurityTab(parent)
     local panel = vgui.Create("DPanel", parent)
     panel.Paint = function(self, w, h) draw.RoundedBox(4, 0, 0, w, h, Color(255, 255, 255, 255)) end
 
-    -- Добавьте элементы для вкладки "Безопасность" здесь
+    -- Добавлю потом элементы
 
     return panel
 end
